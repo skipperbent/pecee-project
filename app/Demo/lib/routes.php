b07ed4ebe89d065cd331ecadd4652c8338a107f2
Simon Sessingø <simon.sessingoe@gmail.com>
2015-10-21 19:20:56 +0200

    [TASK] Added custom CsrfVerifier middleware.

--- app/Demo/lib/routes.php
+++ app/Demo/lib/routes.php
@@ -5,6 +5,8 @@
 
 use Pecee\Router;
 
+Router::csrfVerifier(new \Demo\Middleware\CsrfVerifier());
+
 Router::get('/', 'ControllerDefault@index')->setAlias('home');
 Router::get('/contact', 'ControllerDefault@contact')->setAlias('contact');
 Router::match(['get', 'post'], '/companies/{id}', 'ControllerDefault@companies')->setAlias('companies');

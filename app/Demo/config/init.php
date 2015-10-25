75825a227fc42e99dc7e11b982ed0bc169c6edd7
Simon Sessingø <simon.sessingoe@gmail.com>
2015-10-25 21:15:19 +0100

    Update init.php

--- app/Demo/config/init.php
+++ app/Demo/config/init.php
@@ -1,5 +1,5 @@
 <?php
-$abspath = substr(__FILE__,0,strlen(__FILE__) - strlen('config/init.php'));
+$abspath = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 set_include_path($abspath . PATH_SEPARATOR . $abspath . 'lib' . DIRECTORY_SEPARATOR);
 require_once  '../../../vendor/pecee/framework/config/routes.php';
 require_once 'config/config.php';

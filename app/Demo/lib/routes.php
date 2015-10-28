c22655b31c49c153b54f3772299856dbf8dcf03d
Simon Sessingø <simon.sessingoe@gmail.com>
2015-10-28 06:16:16 +0100

    [BUGFIX] Updated routes.php

--- app/Demo/lib/routes.php
+++ app/Demo/lib/routes.php
@@ -9,6 +9,7 @@ Router::csrfVerifier(new \Demo\Middleware\CsrfVerifier());
 
 Router::get('/', 'ControllerDefault@index')->setAlias('home');
 Router::get('/contact', 'ControllerDefault@contact')->setAlias('contact');
+Router::match(['get', 'post'], '/companies', 'ControllerDefault@companies')->setAlias('companies');
 Router::match(['get', 'post'], '/companies/{id}', 'ControllerDefault@companies')->setAlias('companies');
 
 Router::addExceptionHandler('\Demo\Handler\CustomExceptionHandler');

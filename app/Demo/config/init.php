b7436005cbcc410e3b4873a55bcd907a519e7a37
Simon Sessingø <simon.sessingoe@gmail.com>
2015-12-31 06:01:11 +0100

    Update init.php
    No need to use include path.

--- app/Demo/config/init.php
+++ app/Demo/config/init.php
@@ -2,4 +2,4 @@
 $abspath = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 set_include_path($abspath . PATH_SEPARATOR . $abspath . 'lib' . DIRECTORY_SEPARATOR);
 require_once  '../../../vendor/pecee/framework/config/routes.php';
-require_once 'config/config.php';
+require_once 'config.php';

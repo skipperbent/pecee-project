a4428a538b0b5531c1d95287f229c81a673da6bd
Simon Sessingø <simon.sessingoe@gmail.com>
2016-03-31 15:37:40 +0200

    [TASK] Updated bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -3,3 +3,26 @@ $abspath = __DIR__ . DIRECTORY_SEPARATOR;
 set_include_path($abspath . PATH_SEPARATOR . $abspath . 'app' . DIRECTORY_SEPARATOR);
 require_once  'vendor/pecee/framework/boot.php';
 require_once 'config/config.php';
+
+$modules = \Pecee\Module::getInstance();
+
+function modules_autoloader($class) {
+    global $modules;
+    $file = explode('\\', $class);
+    $app = array_shift($file);
+    $file = join(DIRECTORY_SEPARATOR, $file) . '.php';
+
+    $module = $modules->get($app);
+
+    if($module !== null) {
+        require_once $module . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . $file;
+    }
+}
+
+if(count($modules->getList())) {
+    foreach($modules->getList() as $module) {
+        set_include_path(get_include_path() . PATH_SEPARATOR . $module . '/app/' . PATH_SEPARATOR . $module . '/');
+    }
+}
+
+spl_autoload_register('modules_autoloader');

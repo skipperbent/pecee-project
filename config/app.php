e5e729b64b6f04354d739e53e6898fc2958d2cb1
Simon Sessingø <simon.sessingoe@gmail.com>
2016-09-27 21:49:35 +0200

    - Database support is now disabled if no DB_DRIVER env variable was found. - Optimised module-loader in bootstrap. - Updated composer dependencies.

--- config/app.php
+++ config/app.php
@@ -1,7 +1,5 @@
 <?php
 
-use \Pecee\Translation;
-
 /* ---------- Configuration start ---------- */
 
 // Example usage: Registry
@@ -9,20 +7,25 @@ use \Pecee\Translation;
 // $key->set('StuffToSave', 'ValueToRetrieve');
 
 // Set the framework to use XML for language
+use \Pecee\Translation;
 Translation::getInstance()->setType(Translation::TYPE_XML);
 
 
 // ---- DATABASE SETTINGS ----
 
-$app['db'] = array(
-    'driver'    => env('DB_DRIVER'),
-    'host'      => env('DB_HOST'),
-    'database'  => env('DB_DATABASE'),
-    'username'  => env('DB_USERNAME'),
-    'password'  => env('DB_PASSWORD'),
-    'charset'   => 'utf8', // Optional
-    'collation' => 'utf8_unicode_ci', // Optional
-    'prefix'    => '', // Table prefix, optional
-);
+if(env('DB_DRIVER') !== null) {
+
+    $app['db'] = array(
+        'driver' => env('DB_DRIVER'),
+        'host' => env('DB_HOST'),
+        'database' => env('DB_DATABASE'),
+        'username' => env('DB_USERNAME'),
+        'password' => env('DB_PASSWORD'),
+        'charset' => 'utf8', // Optional
+        'collation' => 'utf8_unicode_ci', // Optional
+        'prefix' => '', // Table prefix, optional
+    );
+
+}
 
 /* ---------- Configuration end ---------- */

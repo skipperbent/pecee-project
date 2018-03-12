a32dcdbf400d87d557b974254ebd1070ab23c04a
Simon Sessingø <simon.sessingoe@gmail.com>
2018-03-12 13:27:54 +0100

    Updates

--- app/Handler/ExceptionHandler.php
+++ app/Handler/ExceptionHandler.php
@@ -12,7 +12,7 @@ class ExceptionHandler extends \Pecee\Handler\ExceptionHandler
     public function handleError(Request $request, \Exception $error)
     {
         // Return json errors if we encounter an error on the API.
-        if (stripos($request->getUri(), '/api') !== false) {
+        if (stripos($request->getUrl()->getPath(), '/api') !== false) {
             response()->json(['error' => $error->getMessage()]);
         }
 
--- config/app.php
+++ config/app.php
@@ -14,21 +14,21 @@ app()->setAdminIps([
     '127.0.0.1',
 ]);
 
-
 // ---- DATABASE SETTINGS ----
 
-if(env('DB_DRIVER') !== null) {
+if (env('DB_DRIVER') !== null) {
 
-    $app['db'] = array(
-        'driver' => env('DB_DRIVER'),
-        'host' => env('DB_HOST'),
-        'database' => env('DB_DATABASE'),
-        'username' => env('DB_USERNAME'),
-        'password' => env('DB_PASSWORD'),
-        'charset' => 'utf8mb4', // Optional
-        'collation' => 'utf8mb4_unicode_ci', // Optional
-        'prefix' => '', // Table prefix, optional
-    );
+    $app['db'] = [
+        'driver'            => env('DB_DRIVER'),
+        'host'              => env('DB_HOST'),
+        'database'          => env('DB_DATABASE'),
+        'username'          => env('DB_USERNAME'),
+        'password'          => env('DB_PASSWORD'),
+        'charset'           => 'utf8mb4', // Optional
+        'collation'         => 'utf8mb4_unicode_ci', // Optional
+        'prefix'            => '', // Table prefix, optional
+        'query_overwriting' => true,
+    ];
 
 }
 
